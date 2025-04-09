c2091b84a6d76bfb68fa9b4a4c0819f4f9982eaf
johnwallacegerrard <levijohnbailey@gmail.com>
2025-04-09 11:44:14 -0400

    fix not found route error handling, and custom error messages

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,12 @@ const clothingItemRouter = require("./clothingItems");
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 router.use((req, res) => {
-  res.status(200).send(req.body);
+  if (res.status === 200) {
+    return res.send(req.body);
+  }
+  if (res.status === 404) {
+    return res.send({ message: "route not found" });
+  }
 });
 
 module.exports = router;
--- utils/errors.js
+++ utils/errors.js
@@ -1,11 +1,11 @@
 const BAD_REQUEST = (err, res) => {
-  res.status(400).send({ message: err.message });
+  res.status(400).send({ message: "bad request" });
 };
 const NOT_FOUND = (err, res) => {
-  res.status(404).send({ message: err.message });
+  res.status(404).send({ message: "document not found" });
 };
 const SERVER_ERROR = (err, res) => {
-  res.status(500).send({ message: err.message });
+  res.status(500).send({ message: "an error has occured on the server" });
 };
 
 module.exports = { BAD_REQUEST, NOT_FOUND, SERVER_ERROR };
