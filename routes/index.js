ea5de7decc1c8941fe2358ff7e62c0e958400135
johnwallacegerrard <levijohnbailey@gmail.com>
2025-04-10 12:17:29 -0400

    attach NOT_FOUND function to routes not found in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
-router.use((req, res) => res.send({ message: "route not found" }));
+router.use((req, res) => NOT_FOUND(req, res));
 
 module.exports = router;
