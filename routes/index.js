d404433119e7dfbcb37edd893a0baa10d256d15f
johnwallacegerrard <levijohnbailey@gmail.com>
2025-04-10 11:37:35 -0400

    fix lint script in package.json and remove if statements from the 404 route handler in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,6 @@ const clothingItemRouter = require("./clothingItems");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
-router.use((req, res) => {
-  if (res.status === 200) {
-    return res.send(req.body);
-  }
-  if (res.status === 404) {
-    return res.send({ message: "route not found" });
-  }
-});
+router.use((req, res) => res.send({ message: "route not found" }));
 
 module.exports = router;
