69b72bccd4a88fe86cc9059c0b13f3b3cc52c4d9
johnwallacegerrard <levijohnbailey@gmail.com>
2025-06-25 11:51:39 -0400

    remove unexpected console statement

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-  console.log(err._statusCode);
   const { _statusCode, message } = err;
   res.status(_statusCode).send({
     message: _statusCode === 500 ? "An error occurred on the server" : message,
