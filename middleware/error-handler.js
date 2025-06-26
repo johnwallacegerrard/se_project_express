90a04e5760ff58d0a33d56aba8ebe281e936ed39
johnwallacegerrard <levijohnbailey@gmail.com>
2025-06-26 15:16:39 -0400

    revert error handler fix

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
+  console.error(err);
   const { _statusCode = 500, message } = err;
   res.status(_statusCode).send({
     message: _statusCode === 500 ? "An error occurred on the server" : message,
